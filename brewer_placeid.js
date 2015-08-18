8b6c9984b4610c41f188124eb81fac134223f0c2
Michael J. Cox <mike.jon.cox@gmail.com>
2015-08-17 19:17:43 -0600

    initial setup for callback. need to explore promises

--- brewer_placeid.js
+++ brewer_placeid.js
@@ -5,19 +5,27 @@ var deets = pl.placeDetailsRequest;
 
 var params = { input: '10 Barrel Brewing Co'};
 
-function findPlaceId(params) {
+function findPlaceId(paramsi, cb) {
 	ac(params, function(err, res) {
 		if (err) throw err;
+		results = [];
 		for (i = 0; i < res.predictions.length; i++)
 		{
+			place_details = {};
+			place_details.description = res.predictions[i].description;
+			place_details.place_id = res.predictions[i].place_id;
 			console.log(res.predictions[i].description);
 			console.log(res.predictions[i].place_id);
 			debugger;
 			deets({ placeid: res.predictions[i].place_id }, function(err, res) {
+				place_details.vicinity = res.result.vicinity;
+				place_details.website = res.result.website;	
 				console.log(res.result.vicinity);
 				console.log(res.result.website);
 			});
+			results.push(place_details);
 		};
+		cb(results);
 	});
 };
 
